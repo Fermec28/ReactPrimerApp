631c77a683d57041fa4622eedcba8a08f139dcf8
Jose Medina <fermed28@gmail.com>
2018-03-11 21:26:54 -0500

    se ajusta los comentarios a los solicitados

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hola Maker!</h1>
-        <p>Te cuento que este es mi primer componente</p>
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
       </div>
     );
   }
